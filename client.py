439156008e785d689f5d9a24a249193103959085
Дениc Янбулатов <yanbulatov.denis@gmail.com>
2025-09-29 05:15:34 +0700

    fix log

--- client.py
+++ client.py
@@ -145,4 +145,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logger.info("Server stopped by user")
+        logger.info("Interrupted by user")
